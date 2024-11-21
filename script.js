3c9f73568dbca3537d458d939434b019ed31816d
Rupesh Sagaram <s.v.rupesh97@gmail.com>
2024-11-21 09:11:19 +0530

    added function that displays each book as a card on the page

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const myLibrary = [];
 
-
-/* this is a constructor that will be used 
-      in the addBookToLibrary  function. 
+/* this is a constructor that will be used
+in the addBookToLibrary function.
 */
 function Book(title, author,pages){
 this.title = title;
@@ -11,11 +10,21 @@ this.pages = pages;
 }
 
 function addBookToLibrary(title,author,pages){
-      
-      myLibrary.push(new Book(title,author,pages));
+    myLibrary.push(new Book(title,author,pages));
 }
 
 addBookToLibrary('bP','J.k.r',200);
 addBookToLibrary('jP','J.k.r',2300);
-console.log(myLibrary.jP);
+addBookToLibrary('gP','t.k.r',3300);
+addBookToLibrary('Percy jackson','Rick Riorden',1200);
+console.log(myLibrary);
 
+myLibrary.forEach(function(item){
+let divElement = document.createElement("div");
+divElement.classList = "card-body";
+divElement.textContent = `Title: ${item.title}
+Author: ${item.author},
+Pages: ${item.pages} `;
+
+document.body.appendChild(divElement);
+});
