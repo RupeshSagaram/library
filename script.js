0707617c2ca01e02f5ade0e28fc3f70b51e8e389
Rupesh Sagaram <s.v.rupesh97@gmail.com>
2024-11-22 11:06:09 +0530

    added a new book button that dialog and allows user to create and add a new book to the page

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const dialogElement = document.getElementById("dialogElement");
+const newBook = document.getElementById("newBook");
+const confirmBtn = document.getElementById("confirmBtn");
+const inputEl = document.getElementsByClassName("input");
+const cancelBtn = document.getElementById("cancelBtn");
+const bookNameEl = document.forms["my-form"].bookName;
+const authorNameEl = document.forms["my-form"].authorName;
+const pagesEl = document.forms["my-form"].pages;
+const output = document.getElementById("output");
+
 const myLibrary = [];
 
 /* this is a constructor that will be used
@@ -19,12 +29,41 @@ addBookToLibrary('gP','t.k.r',3300);
 addBookToLibrary('Percy jackson','Rick Riorden',1200);
 console.log(myLibrary);
 
-myLibrary.forEach(function(item){
-let divElement = document.createElement("div");
-divElement.classList = "card-body";
-divElement.textContent = `Title: ${item.title}
-Author: ${item.author},
-Pages: ${item.pages} `;
-
-document.body.appendChild(divElement);
+newBook.addEventListener("click", () =>{
+    dialogElement.showModal();
 });
+
+cancelBtn.addEventListener("click", () => {
+    dialogElement.close();
+});
+
+confirmBtn.addEventListener("click", (event)=>{
+    event.preventDefault();
+    dialogElement.close();
+});
+
+dialogElement.addEventListener("close",()=>{
+    addBookToLibrary(bookNameEl.value,authorNameEl.value,pagesEl.value);
+        let divElement = document.createElement("div");
+        divElement.classList = "card-body";
+        divElement.textContent = `Title: ${bookNameEl.value}
+        Author: ${authorNameEl.value},
+        Pages: ${pagesEl.value} `;
+        
+        document.body.appendChild(divElement);
+    
+});
+
+myLibrary.forEach(function(item){
+    let divElement = document.createElement("div");
+    divElement.classList = "card-body";
+    divElement.textContent = `Title: ${item.title}
+    Author: ${item.author},
+    Pages: ${item.pages} `;
+    
+    document.body.appendChild(divElement);
+    });
+
+
+
+    
