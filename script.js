6fa8e02ae655facff703903ad84a64f952b03c85
Rupesh Sagaram <s.v.rupesh97@gmail.com>
2024-11-23 23:10:00 +0530

    added remove button to each book, each change will reflect in an array aswell

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const authorNameEl = document.forms["my-form"].authorName;
 const pagesEl = document.forms["my-form"].pages;
 const output = document.getElementById("output");
 
+
+
 const myLibrary = [];
 
 /* this is a constructor that will be used
@@ -42,28 +44,74 @@ confirmBtn.addEventListener("click", (event)=>{
     dialogElement.close();
 });
 
-dialogElement.addEventListener("close",()=>{
-    addBookToLibrary(bookNameEl.value,authorNameEl.value,pagesEl.value);
+dialogElement.addEventListener("close",() =>{
+    console.log(myLibrary);
         let divElement = document.createElement("div");
+        divElement.dataset.bookName = bookNameEl.value;
+        divElement.dataset.authorName = authorNameEl.value;
+        divElement.dataset.pages = pagesEl.value;
+        //creating data attributes to elements
+        const bookName = divElement.dataset.bookName;
+        const authorName = divElement.dataset.authorName;
+        const pages = divElement.dataset.pages;
+
+        addBookToLibrary(bookName,authorName, pages);
+        console.log(divElement.dataset.bookName);
+        console.log(divElement.dataset.authorName);
+        console.log(divElement.dataset.pages);
+        console.log(myLibrary);
+        let removeBtn = document.createElement("button");
+        removeBtn.textContent = "Remove Button";
+        removeBtn.classList = "removeBtn";
         divElement.classList = "card-body";
         divElement.textContent = `Title: ${bookNameEl.value}
         Author: ${authorNameEl.value},
         Pages: ${pagesEl.value} `;
-        
         document.body.appendChild(divElement);
+        divElement.appendChild(removeBtn);
     
 });
 
+
+
 myLibrary.forEach(function(item){
     let divElement = document.createElement("div");
+    let removeBtn = document.createElement("button");
+        removeBtn.textContent = "Remove Button";
+        removeBtn.classList = "removeBtn";
     divElement.classList = "card-body";
+        
     divElement.textContent = `Title: ${item.title}
     Author: ${item.author},
     Pages: ${item.pages} `;
     
     document.body.appendChild(divElement);
+    divElement.appendChild(removeBtn);
+
+
     });
 
+    function handleRemoveButtonClick(event){
+        const divElement = event.target.parentElement;
+        delete divElement.dataset.bookName;
+        delete divElement.dataset.authorName;
+        delete divElement.dataset.pages;
+
+        const index = myLibrary.findIndex(obj =>{
+            obj.title === divElement.dataset.bookName; //to remove item from array, we find the index of the obj then use splice method.
+        });
+
+        myLibrary.splice(index,1);
+      
+        divElement.remove(); // this will remove the entire element
+    }
+
+    document.body.addEventListener("click", (event) => {
+            if (event.target && event.target.classList.contains("removeBtn")) {
+                handleRemoveButtonClick(event);
+            }
+            console.log(myLibrary);
+    });
 
 
     
